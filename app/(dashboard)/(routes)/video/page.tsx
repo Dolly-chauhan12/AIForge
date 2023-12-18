fa4bbc9269335bf456951c6656eabb0d9c793c93
Dolly <abcddolly1234@gmail.com>
2023-12-18 22:34:21 +0530

    Added Text-to-video generation part

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./constants";
+import { Button } from "@/components/ui/button";
+import Empty from "@/components/Empty";
+import Loader from "@/components/Loader";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import Heading from "@/components/Heading";
+import { Music4 } from "lucide-react";
+
+const Musicpage = () => {
+  const router = useRouter();
+
+  const [video, setVideo] = useState<string>();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+
+      const response = await axios.post("/api/video", values);
+
+      console.log(response);
+      setVideo(response.data[0]);
+
+      form.reset();
+    } catch (error: any) {
+      //TODO : Pro Modal popup
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+  return (
+    <div>
+      <Heading
+        title="Video"
+        icon={Music4}
+        description="Create video from your prompt"
+        bgColor="bg-orange-500/10"
+        iconColor="text-orange-500"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg
+            border w-full p-4 px-3 md:px-6
+            focus-within:shadow-sm
+            grid grid-cols-12 gap-2"
+            >
+              <FormField
+                control={form.control}
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        placeholder="Clown fish swimming in a coral reef"
+                        disabled={isLoading}
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                size="icon"
+                disabled={isLoading}
+                type="submit"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {!video && !isLoading && <Empty label="No frames generated..." />}
+          {video && (
+            <video
+              className="w-full mt-8 aspect-video rounded-lg border bg-black"
+              controls
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Musicpage;
